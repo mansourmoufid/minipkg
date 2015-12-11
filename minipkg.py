235a36c070105f615f943a2a3579a344303c04cd
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-11 16:08:23 -0500

    Bump to version 0.7.

--- minipkg.py
+++ minipkg.py
@@ -19,7 +19,7 @@ __copyright__ = 'Copyright 2015, Mansour Moufid'
 __email__ = 'mansourmoufid@gmail.com'
 __license__ = 'ISC'
 __status__ = 'Development'
-__version__ = '0.6'
+__version__ = '0.7'
 
 
 supported_sys = ('Linux', 'Darwin')
