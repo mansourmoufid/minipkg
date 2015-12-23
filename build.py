efcdfd58c671001c2a71cca5f1242cade3fd5d98
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-23 16:37:13 -0500

    Generate a pkg_summary.gz file.

--- build.py
+++ build.py
@@ -49,3 +49,29 @@ if __name__ == '__main__':
         os.environ.update({
             'PATH': var + os.pathsep + os.environ['PATH'],
         })
+
+    pkgnames = [
+        pkg.split(' ')[1] if ' ' in pkg else pkg.split('/')[1]
+        for pkg in pkgs
+    ]
+    p = subprocess.Popen(
+        ['pkg_info', '-X'] + pkgnames,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, err = p.communicate()
+    assert p.returncode == 0, 'pkg_info'
+    lines = out.split('\n')
+    info = filter(lambda s: not 'REQUIRES=' in s, lines)
+    pkg_summary = os.path.join(localbase, 'packages', 'pkg_summary')
+    os.remove(pkg_summary)
+    os.remove(pkg_summary + '.gz')
+    with open(pkg_summary, 'w+') as f:
+        print('\n'.join(info), file=f)
+    p = subprocess.Popen(
+        ['gzip', pkg_summary],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, err = p.communicate()
+    assert p.returncode == 0, 'gzip'
