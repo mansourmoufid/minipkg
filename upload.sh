c48a78c03d7598c7542933b19b1348a14b4432f0
Mansour Moufid <mansourmoufid@gmail.com>
2016-01-17 02:14:33 -0500

    Add a script to upload binary packages.

---
+++ upload.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+SYS=$(uname -s)
+ARCH=$(uname -m)
+LOCAL="$HOME/usr/pkgsrc/packages"
+REMOTE="s3://minipkg.eliteraspberries.com/packages"
+OPTS="--acl public-read --cache-control 'max-age=3600'"
+aws s3 cp $OPTS $LOCAL/pkg_summary.gz $REMOTE/$SYS/$ARCH/
+aws s3 cp --recursive $OPTS $LOCAL/All $REMOTE/$SYS/$ARCH/All/
