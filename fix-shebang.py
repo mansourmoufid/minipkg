ea30b5bbe44a2c26147e8d263f30e9ff420d71be
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-01 00:49:03 -0400

    Add a script to fix interpreter paths.

---
+++ fix-shebang.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+"""fix-shebang.py - Fix interpreter paths
+"""
+
+
+from __future__ import print_function
+
+import os
+import stat
+import sys
+import tempfile
+
+
+def read_shebang(f):
+    interp = ''
+    x = f.read(2)
+    if x == '#!':
+        for i in range(1024):
+            c = f.read(1)
+            if c == '' or c == '\n':
+                break
+            interp += c
+    return interp
+
+
+def issystem(path):
+    system_dirs = [
+        '/bin',
+        '/opt',
+        '/usr',
+        '/Library',
+        '/System',
+    ]
+    return any(path.startswith(dir) for dir in system_dirs)
+
+
+def parse_shebang(shebang):
+    env = shebang.split()
+    first = env[0]
+    if issystem(first):
+        return env
+    if os.path.basename(first) == 'env':
+        interp = env[1]
+        interp = os.path.basename(interp)
+        return ['/usr/bin/env', interp] + env[2:]
+    if os.path.exists(first):
+        interp = first
+        interp = os.path.basename(interp)
+        return ['/usr/bin/env', interp] + env[1:]
+    return env
+
+
+if __name__ == '__main__':
+
+    for line in sys.stdin:
+
+        path = line.rstrip('\n')
+        if path == '':
+            continue
+        if not os.path.isfile(path):
+            continue
+
+        st = os.stat(path)
+        if st.st_size < len('#!.\n'):
+            continue
+        mode = st.st_mode
+        try:
+            os.chmod(path, mode | stat.S_IRUSR | stat.S_IWUSR)
+        except OSError:
+            continue
+
+        with open(path, 'r') as f:
+            shebang = read_shebang(f)
+            if shebang == '':
+                continue
+            env = parse_shebang(shebang)
+            print(path, env)
+            tmp, tmpname = tempfile.mkstemp()
+            line = '#!' + ' '.join(env) + '\n'
+            os.write(tmp, line)
+            while True:
+                x = f.read(4096)
+                if x == '':
+                    break
+                os.write(tmp, x)
+            os.close(tmp)
+        os.rename(tmpname, path)
+
+        os.chmod(path, mode)
