fbb086c80b3cdd03dc2f9c73ba0d96d24780d910
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-14 20:45:59 -0400

    Bump to version 1.8.

--- minipkg.py
+++ minipkg.py
@@ -30,7 +30,7 @@ __copyright__ = 'Copyright 2015, 2016, Mansour Moufid'
 __email__ = 'mansourmoufid@gmail.com'
 __license__ = 'ISC'
 __status__ = 'Development'
-__version__ = '1.7'
+__version__ = '1.8'
 
 
 supported_sys = ('Linux', 'Darwin')
