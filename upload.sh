322e8c85c7dff248df27e5b11c5e2d62208a3e55
Mansour Moufid <mansourmoufid@gmail.com>
2016-06-19 21:09:00 -0400

    Set max-age directive to one day.

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@ SYS=$(uname -s)
 ARCH=$(uname -m)
 LOCAL="$HOME/usr/pkgsrc/packages"
 REMOTE="s3://minipkg.eliteraspberries.com/packages"
-OPTS="--acl public-read --cache-control 'max-age=3600'"
+OPTS="--acl public-read --cache-control max-age=86400"
 aws s3 cp $OPTS $LOCAL/pkg_summary.gz $REMOTE/$SYS/$ARCH/
 aws s3 cp --recursive $OPTS $LOCAL/All $REMOTE/$SYS/$ARCH/All/
