74202a010920c271d5e166f2dd83bae4db81abfc
Mansour Moufid <mansourmoufid@gmail.com>
2018-09-08 15:46:12 -0400

    Do not install binary packages.

--- minipkg.py
+++ minipkg.py
@@ -102,77 +102,11 @@ def extract(tgz, path):
         subprocess.check_call(['gunzip', tgz])
     subprocess.check_call(['tar', '-xf', tar, '-C', path])
 
-
-all_packages = [
-    'alt-ergo-1.01nb2.tgz',
-    'autoconf-2.69nb6.tgz',
-    'automake-1.15nb3.tgz',
-    'bzip2-1.0.6nb1.tgz',
-    'ca-certificates-20161003.tgz',
-    'camlp4-4.02.7.tgz',
-    'clang-3.8.0nb6.tgz',
-    'cmake-3.7.0nb2.tgz',
-    'coccinelle-1.0.6nb2.tgz',
-    'curl-7.52.1.tgz',
-    'cython-0.24nb2.tgz',
-    'digest-20160304.tgz',
-    'fftw-3.3.6nb2.tgz',
-    'frama-c-20151002nb3.tgz',
-    'gettext-lib-0.19.8.1.tgz',
-    'gettext-tools-0.19.8.1.tgz',
-    'gmake-4.2.1.tgz',
-    'gmp-6.1.0nb3.tgz',
-    'gsed-4.2.2nb4.tgz',
-    'gzip-1.6.tgz',
-    'help2man-1.47.3.tgz',
-    'jpeg-9bnb2.tgz',
-    'libarchive-3.1.2.tgz',
-    'libcxx-3.8.0nb3.tgz',
-    'libffi-3.2.1nb3.tgz',
-    'libpng-1.6.28.tgz',
-    'libtool-base-2.4.2nb12.tgz',
-    'lzip-1.17.tgz',
-    'mbedtls-2.4.0nb3.tgz',
-    'menhir-20151112nb4.tgz',
-    'nbpatch-20151107.tgz',
-    'ncurses-6.0nb4.tgz',
-    'ocaml-4.02.3nb9.tgz',
-    'ocaml-findlib-1.6.1nb5.tgz',
-    'ocamlgraph-1.8.7nb2.tgz',
-    'p5-gettext-1.07nb1.tgz',
-    'parmap-1.0rc7nb5.tgz',
-    'pcre-8.39.tgz',
-    'pcre-ocaml-7.2.2nb3.tgz',
-    'perl-5.24.0.tgz',
-    'pkgconf-0.9.12.20151211nb2.tgz',
-    'portaudio-190600_20161030nb2.tgz',
-    'python-2.7.13nb7.tgz',
-    'python-numpy-1.11.0nb4.tgz',
-    'python-pillow-3.3.0nb3.tgz',
-    'python-setuptools-28.8.0.tgz',
-    'python-wheel-0.29.0.tgz',
-    'readline-6.3nb3.tgz',
-    'scons-2.5.1.tgz',
-    'sqlite3-3.13.0.tgz',
-    'tcl-8.6.5nb2.tgz',
-    'tk-8.6.5.tgz',
-    'xz-5.2.2.tgz',
-    'zarith-1.4.1nb7.tgz',
-    'zlib-1.2.8nb3.tgz',
-]
-
 recommended_packages = [
     'digest-*',
     'nbpatch-*',
     'clang-*',
 ]
-
-
-def install_binary_package(home, pkg):
-    prefix = os.path.join(home, 'pkg')
-    pkg_add = os.path.join(home, 'pkg', 'sbin', 'pkg_add')
-    pkg_path = os.path.join(home, 'usr', 'pkgsrc', 'packages', 'All', pkg)
-    subprocess.check_call([pkg_add, '-I', '-p', prefix, pkg_path])
 
 
 cwd = os.path.split(os.path.abspath(__file__))[0]
@@ -357,20 +291,5 @@ if __name__ == '__main__':
         with open(os.path.join(dotfile), 'a+') as f:
             print('', file=f)
             print('. $HOME/.minipkg_profile', file=f)
-
-    # Step 6:
-    # Install binary packages.
-    print('minipkg: fetching packages ...')
-    repo = '/'.join([host, 'packages', OPSYS, mach])
-    for pkg in all_packages:
-        print('minipkg: fetching', pkg, '...')
-        fetch(
-            '/'.join([repo, 'All', pkg]),
-            path=os.path.join(localbase, 'packages', 'All', pkg),
-            hash=None,
-        )
-    for pkg in recommended_packages:
-        print('minipkg: installing', pkg, '...')
-        install_binary_package(HOME, pkg)
 
     print('minipkg: done!')
