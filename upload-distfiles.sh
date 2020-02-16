44b0626abf2d63932b781f55d590086f191449cc
Mansour Moufid <mansourmoufid@gmail.com>
2020-02-15 20:50:55 -0500

    Remove obsolete scripts.

--- upload-distfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-aws s3 cp --recursive \
-	$HOME/usr/pkgsrc/distfiles \
-	s3://minipkg.eliteraspberries.com/distfiles \
-	--acl public-read \
-	--cache-control max-age=86400 \
-	--exclude CVS/* --exclude .cvsignore
--- upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-SYS=$(uname -s)
-ARCH=$(uname -m)
-LOCAL="$HOME/usr/pkgsrc/packages"
-REMOTE="s3://minipkg.eliteraspberries.com/packages"
-OPTS="--acl public-read --cache-control max-age=86400"
-aws s3 cp $OPTS $LOCAL/pkg_summary.gz $REMOTE/$SYS/$ARCH/
-aws s3 cp --recursive $OPTS $LOCAL/All $REMOTE/$SYS/$ARCH/All/
