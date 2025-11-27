421a15ed7b417a4779879c339ce58dcecd17c433
Mansour Moufid <mansourmoufid@gmail.com>
2025-11-27 18:48:04 -0500

    Refactor rpath handling.

--- fix-rpath.py
+++ fix-rpath.py
@@ -8,8 +8,6 @@ Usage:
 """
 
 
-from __future__ import print_function
-
 import functools
 import os
 import platform
@@ -23,13 +21,13 @@ Popen = functools.partial(subprocess.Popen, universal_newlines=True)
 
 
 __author__ = 'Mansour Moufid'
-__copyright__ = 'Copyright 2015, 2016, Mansour Moufid'
+__copyright__ = 'Copyright 2015-2025, Mansour Moufid'
 __email__ = 'mansourmoufid@gmail.com'
 __license__ = 'ISC'
 __status__ = 'Development'
 
 
-def isexe(exe_pat, path):
+def isexe(path):
     p = Popen(
         ['file', path],
         stdout=subprocess.PIPE,
@@ -39,17 +37,18 @@ def isexe(exe_pat, path):
     if not p.returncode == 0:
         return False
     x = out.split(': ')[1]
-    return exe_pat.match(x) is not None
+    return re.match(r'^Mach-O |^ELF ', x) is not None
 
 
 def islib(path):
     return os.path.isfile(path) and (
         path.endswith('.so') or
-        path.endswith('.dylib')
+        path.endswith('.dylib') or
+        path.endswith('.bundle')
     )
 
 
-def install_names(bin):
+def loaded_libs(bin):
     p = Popen(
         ['otool', '-L', bin],
         stdout=subprocess.PIPE,
@@ -58,11 +57,11 @@ def install_names(bin):
     out, _ = p.communicate()
     assert p.returncode == 0, 'otool -L %s' % bin
     lines = out.split('\n')
-    names = [
+    libs = [
         line.split()[0] for line in lines
         if line and not line.endswith(':')
     ]
-    return names
+    return libs
 
 
 def issystem(path):
@@ -82,7 +81,7 @@ def path_strip(prefix, path):
     return path.split(prefix)[-1]
 
 
-def change_id_name(lib, id):
+def set_id_name(lib, id):
     ret = subprocess.call(['install_name_tool', '-id', id, lib])
     assert ret == 0, 'install_name_tool -id %s %s' % (id, lib)
 
@@ -97,10 +96,7 @@ def change_install_name(bin, old, new):
 
 
 def fix_rpath_lib(prefix, lib):
-    basename = path_strip(prefix, lib)
-    basename = path_strip('@rpath', basename)
-    rpath = os.path.join('@rpath', basename)
-    change_id_name(lib, rpath)
+    set_id_name(lib, re.sub('.*{}'.format(prefix), r'@rpath', lib))
 
 
 def add_rpath(bin, path):
@@ -119,39 +115,34 @@ def add_rpath(bin, path):
 
 
 def fix_rpath_exe(prefix, exe):
-    cwd = os.path.split(exe)[0]
-    names = install_names(exe)
-    names = [name for name in names if not issystem(name)]
-    for name in names:
-        if name == prefix:
-            continue
-        basename = path_strip('@rpath', name)
-        if basename == os.path.basename(name):
-            basename = os.path.join(cwd, basename)
-        basename = path_strip(prefix, basename)
-        rpath = os.path.join('@rpath', basename)
-        change_install_name(exe, name, rpath)
+    dir = os.path.split(exe)[0]
+    libs = loaded_libs(exe)
+    libs = [lib for lib in libs if not issystem(lib)]
+    for lib in libs:
+        rpath = re.sub('.*{}'.format(prefix), '@rpath', lib)
+        change_install_name(exe, lib, rpath)
     add_rpath(exe, prefix)
 
 
 def relative_path(path, top):
-    path = path_strip(top, path)
+    path = re.sub('.*{}'.format(top), '', path)
     base = path.split(os.path.sep)[:-1]
-    dirs = ['..' for dir in base]
+    dirs = ['..' for dir in base if dir != '']
     return os.path.sep.join(dirs)
 
 
-def add_rpath_loader_path(prefix, bin):
+def add_rpath_loader_path(destdir, prefix, bin):
+    common_prefix = os.path.commonprefix([prefix, re.sub(destdir, '', bin)])
     loader_path = os.path.join(
         '@loader_path',
-        relative_path(bin, prefix),
+        relative_path(re.sub(destdir, '', bin), common_prefix),
     )
     add_rpath(bin, loader_path)
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         if sys.argv[1] in ('-h', '--help'):
             print(__doc__)
             sys.exit(os.EX_OK)
@@ -159,7 +150,8 @@ if __name__ == '__main__':
         print(__doc__)
         sys.exit(os.EX_USAGE)
 
-    prefix = sys.argv[1]
+    destdir = sys.argv[1]
+    prefix = sys.argv[2]
     prefix = os.path.expanduser(prefix)
     prefix = os.path.abspath(prefix)
 
@@ -168,14 +160,10 @@ if __name__ == '__main__':
     if system not in supported_systems:
         print('warning: unsupported system', file=sys.stderr)
         sys.exit(0)
-    system_exe_pat = {
-        'Darwin': 'Mach-O( .*)? (executable|shared library|bundle)',
-    }
-    exe_pat = re.compile(system_exe_pat[system])
 
     for line in sys.stdin:
         path = line.rstrip('\n')
-        if not os.path.exists(path) or not isexe(exe_pat, path):
+        if not os.path.exists(path) or not isexe(path):
             continue
         exe = os.path.abspath(path)
         print(exe)
@@ -185,7 +173,7 @@ if __name__ == '__main__':
             if islib(exe):
                 fix_rpath_lib(prefix, exe)
             fix_rpath_exe(prefix, exe)
-            add_rpath_loader_path(prefix, exe)
+            add_rpath_loader_path(destdir, prefix, exe)
         except AssertionError:
             pass
         os.chmod(exe, mode)
